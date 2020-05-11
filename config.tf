321a6a2dd0d4a2593bbd3d6409cee8a924726dbf
Bliznetsov Artem <git@iamche.ru>
2020-05-11 14:33:17 +0300

    fix

--- config.tf
+++ config.tf
@@ -1,18 +1,20 @@
 provider "aws" {
- region = "us-east-2"
+    region = "us-east-2"
 }
 
+resource ""
+
 resource "aws_instance" "example" {
- ami = "ami-07c1207a9d40bc3bd"
- instance_type = "t2.micro"
- key_name = "myKey"
+    ami = "ami-07c1207a9d40bc3bd"
+    instance_type = "t2.micro"
+    key_name = "myKey"
+    security_groups = "General group"
 
- provisioner "local-exec" {
-    command = "apt update"
- }
- provisioner "local-exec" {
-    command = "apt install -y nginx"
- }
-
+    provisioner "local-exec" {
+        command = "apt update"
+    }
+    provisioner "local-exec" {
+        command = "apt install -y nginx"
+    }
 }
 
