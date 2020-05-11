39fe8a741d843241a333d37a292a25058b2817f2
Bliznetsov Artem <git@iamche.ru>
2020-05-11 14:35:44 +0300

    fix

--- config.tf
+++ config.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
     ami = "ami-07c1207a9d40bc3bd"
     instance_type = "t2.micro"
     key_name = "myKey"
-    security_groups = "General group"
+    security_groups = ["General group"]
 
     provisioner "local-exec" {
         command = "apt update"
